96ebcca6eb3ffe43dc10ec17dc0cc8cd00e800bc
kimzz14 <kimzz1004@gmail.com>
2024-04-03 04:21:59 +0000

    setting

--- 00.export_path.sh
+++ 00.export_path.sh
@@ -10,3 +10,13 @@ else
 	echo "Diff"
 fi
 ##################################################################################
+#conda create -n 'hifiasm-v0.19.8-240104'
+
+#source activate hifiasm-v0.19.8-240104
+
+#conda install hifiasm
+#bioconda/linux-64::hifiasm-0.19.8-h43eeafb_0
+
+#conda install yak
+#bioconda/linux-64::yak-0.1-he4a0461_4
+##################################################################################
